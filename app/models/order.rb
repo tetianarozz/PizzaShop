83f5cc5f9a9d57e69a06384e02618e6743b3d2aa
tetianarozz <tatianarozputnia@gmail.com>
2022-04-22 16:57:19 +0300

    time, total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,4 +16,12 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :line_items
 
   validates :user_name, :user_address, :user_number, presence: true
+
+  def formatted_created_at
+    created_at.strftime("%-d/%m/%Y - %H:%M")
+  end
+
+  def total_price
+    line_items.to_a.sum(&:total_price)
+  end
 end
