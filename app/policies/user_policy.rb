aa3cd79e6b71e7e48109236d3834a8114b7574fe
tetianarozz <tatianarozputnia@gmail.com>
2022-04-26 18:39:10 +0300

    Authorization, gem pundit

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,29 +1,29 @@
 class UserPolicy < ApplicationPolicy
   def index?
-    user.admin?
+    user&.admin?
   end
 
   def show?
-    user.admin?
+    user&.admin?
   end
 
   def create?
-    user.admin?
+    user&.admin?
   end
 
   def new?
-    user.admin?
+    user&.admin?
   end
 
   def update?
-    user.admin?
+    user&.admin?
   end
 
   def edit?
-    user.admin?
+    user&.admin?
   end
 
   def destroy?
-    user.admin?
+    user&.admin?
   end
 end
